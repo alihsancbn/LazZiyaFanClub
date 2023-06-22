5dff1a80d5f3253309ca865359d072f60f79446c
Ali İhsan Çoban <alihsancbn@gmail.com>
2023-06-22 19:41:04 +0300

    init

--- app.js
+++ app.js
@@ -39,9 +39,10 @@ app.use(publicData.routes);
 app.use(hata.goster404);
 
 
-mongoose.connect(MONGODB_URI)
+mongoose
+.connect(MONGODB_URI)
 .then(result => {
-    app.listen(process.env.PORT ||3000);
+    app.listen(process.env.PORT || 3000);
 })
 .catch(err => {
     console.log(err);
